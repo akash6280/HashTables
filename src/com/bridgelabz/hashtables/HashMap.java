a2930ba3c2cb69518bbc29a94ac0c0b145745f8d
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 11:41:39 +0530

    MODIFIED: HashMap changed to contain multple linked list

--- src/com/bridgelabz/hashtables/HashMap.java
+++ src/com/bridgelabz/hashtables/HashMap.java
@@ -1,23 +1,46 @@
 package com.bridgelabz.hashtables;
 
+import java.util.*;
 
 public class HashMap<K,V> {
-	LinkedList<K> linkedList;
+	private int numBuckets;
+	ArrayList<LinkedList<K>> bucketArray;
 	
 	public HashMap() {
-		this.linkedList = new LinkedList<K>();
+		this.numBuckets = 10;
+		this.bucketArray = new ArrayList<>(numBuckets);
+		for(int i=0;i<numBuckets;i++) {
+			bucketArray.add(null);
+		}
+	}
+	
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numBuckets;
+		return index;
 	}
 	
 	public V get (K key) {
-		MapNode<K,V> mapNode = (MapNode<K,V>)linkedList.find(key);
+		int index = this.getBucketIndex(key);
+		LinkedList<K> linkedList = bucketArray.get(index);
+		if(linkedList == null) {
+			return null;
+		}
+		MapNode<K,V> mapNode = (MapNode<K,V>) linkedList.find(key);
 		return (mapNode == null) ? null : mapNode.getValue();
 	}
 	
 	public void add(K key, V value) {
-		MapNode<K,V> mapNode = (MapNode<K,V>)linkedList.find(key);
+		int index = this.getBucketIndex(key);
+		LinkedList<K> linkedList = bucketArray.get(index);
+		if(linkedList == null) {
+			linkedList = new LinkedList<K>();
+			bucketArray.set(index, linkedList);
+		}
+		MapNode<K,V> mapNode = (MapNode<K,V>) linkedList.find(key);
 		if(mapNode == null) {
 			mapNode = new MapNode<K, V>(key,value);
-			this.linkedList.append(mapNode);
+			linkedList.append(mapNode);
 		} else {
 			mapNode.setValue(value);
 		}
