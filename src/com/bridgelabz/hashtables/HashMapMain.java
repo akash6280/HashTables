27437c57f02a29aab9bd788b1c343b166ed64e19
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 11:47:41 +0530

    ADDED: finding frequency of word in large paragraph phrase

--- src/com/bridgelabz/hashtables/HashMapMain.java
+++ src/com/bridgelabz/hashtables/HashMapMain.java
@@ -1,22 +1,23 @@
 package com.bridgelabz.hashtables;
 
 public class HashMapMain {
-
 	public static void main(String[] args) {
-		String sentence= "To be or not to be";
-		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
-		String [] words = sentence.toLowerCase().split(" ");
-		for(String word:words) {
+		
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		HashMap<String, Integer> hashMap = new HashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
 			Integer value = hashMap.get(word);
-			if(value==null) 
-				value=1;
-			else 
-				value=value+1;
+			if(value == null) {
+				value = 1;
+			}
+			else {
+				value += 1;
+			}
 			hashMap.add(word, value);
 		}
-		int frequency = hashMap.get("to");
+		int frequency = hashMap.get("paranoid");
 		System.out.println(frequency);
-		
 	}
 
 }
